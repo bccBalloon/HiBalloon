a2e65976b6cc2e9b1474fbc1153fe117eb7c1cc1
plehrer <plehrer@gmail.com>
2015-05-13 20:02:42 -0400

    Added code to write date and time to file along with headings.

--- readPressure.py
+++ readPressure.py
@@ -1,16 +1,20 @@
 import Adafruit_BBIO.ADC as ADC
 import time
-#import cv2
+import datetime
 
 ADC.setup()
 
-f1 = open('pressure.txt','a')
+f1 = open('pressure.csv','w')
+f1.write("Month,Day,Hour,Minute,Second,Raw Pressure, Pressure (Hg)\n")
+
 
 while 1 :
+    now = datetime.datetime.now()
     raw =  ADC.read("P9_39")
     p = ((raw * 1.8 * 2 -.33)/1.65)*29.92
+
     print str(raw) + ' ' + str(p)
-    f1.write(str(raw)+','+str(p)+'\n') 
+    f1.write(str(now.month)+','+str(now.day)+','+str(now.hour)+','+str(now.minute)+','+str(now.second)+','+str(raw)+','+str(p)+'\n') 
     time.sleep(1)
 f1.close()
 
