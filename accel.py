537ac411de527f3d52b367db19202c94d52c5d8f
plehrer <plehrer@gmail.com>
2015-05-27 13:40:50 -0400

    accel.py: Added code for X,Y,Z axis. Writes raw and calibrated values to separate files.

--- accel.py
+++ accel.py
@@ -4,30 +4,33 @@ import datetime
 
 ADC.setup()
 #zeroOffset Calculated with 100 kOhm resistor
-zeroOffset = 1.466
-#zerroOffset for 500 KOhm
-#zeroOffset = 1.647
+#zeroOffset = 1.466
+#zeroOffset for 500 KOhm
+zeroOffset = 1.647
 #From adxl335 data sheet - sensitivity is 300 mV/g
 conversionFactor = 0.300;
 
-#f1 = open('pressure.csv','w')
-#f1.write("Month,Day,Hour,Minute,Second,Raw Pressure, Pressure (Hg)\n")
-
+f1 = open('acceleration.csv','w')
+f2 = open('rawAcceleartion.csv','w');
+f1.write("Month,Day,Hour,Minute,Second,Z,Y,X\n")
+f2.write("Month,Day,Hour,Minute,Second,Zraw,Yraw,Xraw\n")
 
 while 1 :
     now = datetime.datetime.now()
     rawZ =  ADC.read("P9_40")
-    #rawY = ADC.read("P9_37")
-    #rawX = ADC.read("P9_35")
-    #p = ((raw * 1.8 * 2 -.33)/1.65)*29.92
-
-    #print 'X='+str(rawX) + ' Y=' + str(rawY) + ' Z='+str(rawZ);
+    rawY =  ADC.read("P9_38")
+    rawX =  ADC.read("P9_36")
    
     Zvalue = ((rawZ * 3.6) - zeroOffset)/conversionFactor;
-    # rawZ is multiplied by 3.6 because it has to be multiplied by 1.8 to get voltage and since it is hooked up to a voltage
+    Yvalue = ((rawY * 3.6) - zeroOffset)/conversionFactor;
+    Xvalue = ((rawX * 3.6) - zeroOffset)/conversionFactor;
+    # raw input is multiplied by 3.6 because it has to be multiplied by 1.8 to get voltage and since it is hooked up to a voltage
     # divider it also needs to be multiplied by 2 to get the original voltage
-    print 'Z='+str(Zvalue) #+' Z\'= ' +str(Zvalue); 
-    #f1.write(str(now.month)+','+str(now.day)+','+str(now.hour)+','+str(now.minute)+','+str(now.second)+','+str(raw)+','+str(p)+'\n') 
+    print 'Z = '+str(Zvalue) + ' Y = ' + str(Yvalue) + ' X = ' + str(Xvalue);
+    #print 'Zraw = ' + str(rawZ) + ' Yraw = ' + str(rawY) + ' Xraw = ' + str(rawX);
+    f1.write(str(now.month)+','+str(now.day)+','+str(now.hour)+','+str(now.minute)+','+str(now.second)+','+str(Zvalue)+','+str(Yvalue)+','+str(Xvalue)+'\n');
+    f2.write(str(now.month)+','+str(now.day)+','+str(now.hour)+','+str(now.minute)+','+str(now.second)+','+ str(rawZ)+','+str(rawY)+','+str(rawX)+'\n');
     time.sleep(1)
-#f1.close()
+f1.close()
+f2.close()
 
