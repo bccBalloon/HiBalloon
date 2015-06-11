756d8652ec9890c4b81f431e486cd08d06f9dd5b
plehrer <plehrer@gmail.com>
2015-06-11 14:21:40 -0400

    Corrected calibration by observation and used linear algebra library in numpy to figure norm.

--- accel.py
+++ accel.py
@@ -1,22 +1,23 @@
 import Adafruit_BBIO.ADC as ADC
 import time
 import datetime
-import math
+import numpy as np
 
 ADC.setup()
-#zeroOffset Calculated with 100 kOhm resistor
-#zeroOffset = 1.466
-#zeroOffset for 470 K Ohm
-#zeroOffset = 1.658
 
-# Based on observation
-zeroOffsetZ = 1.68
-zeroOffsetX = 1.588
-zeroOffsetY = 1.604
+zeroOffsetZ = 1.672
+
+# Based on observation of high and low raw readings X 3.6 V. Then took the average of each.
+#zeroOffsetZ = 1.672
+zeroOffsetX = 1.595
+zeroOffsetY = 1.614
 
 
-#From adxl335 data sheet - sensitivity is 300 mV/g
-conversionFactor = 0.314;
+
+#The sensitivity of conversion factor is the average for each axis minus low raw reading.
+conversionFactorZ = 0.322
+conversionFactorY = 0.325
+conversionFactorX = 0.319
 
 f1 = open('acceleration.csv','w')
 f2 = open('rawAcceleration.csv','w');
@@ -29,16 +30,16 @@ while 1 :
     rawY =  ADC.read("P9_38")
     rawX =  ADC.read("P9_36")
    
-    Zvalue = ((rawZ * 3.6) - zeroOffsetZ)/conversionFactor;
-    Yvalue = ((rawY * 3.6) - zeroOffsetY)/conversionFactor;
-    Xvalue = ((rawX * 3.6) - zeroOffsetX)/conversionFactor;
+    Zvalue = ((rawZ * 3.6) - zeroOffsetZ)/conversionFactorZ;
+    Yvalue = ((rawY * 3.6) - zeroOffsetY)/conversionFactorY;
+    Xvalue = ((rawX * 3.6) - zeroOffsetX)/conversionFactorX;
     
     # raw input is multiplied by 3.6 because it has to be multiplied by 1.8 to get voltage and since it is hooked up to a voltage
     # divider it also needs to be multiplied by 2 to get the original voltage
-    #print 'Z = '+str(Zvalue) + ' Y = ' + str(Yvalue) + ' X = ' + str(Xvalue);
-    #norm = math.sqrt(Zvalue * Zvalue + Yvalue * Yvalue + Xvalue * Xvalue);
-    #print 'norm = ' + str(norm);
-    print 'Zraw = ' + str(rawZ * 3.6) + ' Yraw = ' + str(rawY * 3.6) + ' Xraw = ' + str(rawX * 3.6);
+    print 'Z = '+str(Zvalue) + ' Y = ' + str(Yvalue) + ' X = ' + str(Xvalue);
+    a = np.array([Zvalue, Yvalue, Xvalue])
+    print 'Norm = ' + str(np.linalg.norm(a));
+    #print 'Zraw = ' + str(rawZ * 3.6) + ' Yraw = ' + str(rawY * 3.6) + ' Xraw = ' + str(rawX * 3.6);
     f1.write(str(now.month)+','+str(now.day)+','+str(now.hour)+','+str(now.minute)+','+str(now.second)+','+str(Zvalue)+','+str(Yvalue)+','+str(Xvalue)+'\n');
     f2.write(str(now.month)+','+str(now.day)+','+str(now.hour)+','+str(now.minute)+','+str(now.second)+','+ str(rawZ)+','+str(rawY)+','+str(rawX)+'\n');
     time.sleep(1)
