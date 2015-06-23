0481aadda9abad0b8fe7a1e0e3b1b999665da813
plehrer <plehrer@gmail.com>
2015-06-23 15:10:02 -0400

    Initial commit

---
+++ sound.py
@@ -0,0 +1,26 @@
+import Adafruit_BBIO.ADC as ADC
+import time
+
+ADC.setup()
+
+cycles = 10
+soundPin = "P9_33"
+raw = []
+count = 10
+n = 0
+
+f1 = open(rawSound.csv, 'a')
+f1.write("Minimum,Maximum")
+
+
+while count < iter:
+	for x in xrange(0,cycles)
+		raw.append(ADC.read(soundPin))		
+		time.sleep(1/cycles)
+	#print min(raw)
+	#print max(raw)
+	f1.write(str(min(raw))+','+str(max(raw)))
+	print "datapoint"
+	time.sleep(1)
+	count++
+f1.close()
